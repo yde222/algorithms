9b89880bdda69daff183bc3cc603a669b0582c97
yde222 <150855134+yde222@users.noreply.github.com>
2025-05-03 23:08:57 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

--- 250503/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
+++ 250503/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -1,31 +1,45 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        int[] a = new int[n1];
-        int[] b = new int[n2];
-        for (int i = 0; i < n1; i++)
-            a[i] = sc.nextInt();
-        for (int i = 0; i < n2; i++)
-            b[i] = sc.nextInt();
-        
-        if(isSubsequence(a,b))
-            System.out.println("Yes");
-        else
-            System.out.println("No");
+
+    // a 배열에서 start 위치부터 b 배열과 일치하는지 확인
+    private static boolean matchesFrom(int[] a, int[] b, int start) {
+        for (int i = 0; i < b.length; i++) {
+            if (a[start + i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static boolean isSubsequence(int[] x, int[] y){
-        for(int i=0; i<=x.length-y.length;i++ ){
-            int j=0;
-            while(j < y.length && x[i+j]==y[j] ){
-                j++;
-            }
-            if(j==y.length) 
+    // b가 a의 연속 부분 수열인지 확인
+    private static boolean isSubsequence(int[] a, int[] b) {
+        if (b.length > a.length) return false;
+
+        for (int i = 0; i <= a.length - b.length; i++) {
+            if (matchesFrom(a, b, i)) {
                 return true;
+            }
         }
-        return false;      
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+
+        int[] a = new int[n1];
+        int[] b = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            a[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n2; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        System.out.println(isSubsequence(a, b) ? "Yes" : "No");
     }
 }
