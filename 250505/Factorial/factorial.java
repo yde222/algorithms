3f291850b41191a365e66e0f0361f8e233f76d7d
yde222 <150855134+yde222@users.noreply.github.com>
2025-05-05 09:09:46 +0900

    Title: Factorial, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250505/Factorial/factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int Factorial(int n){
+
+        if(n<=1) return 1;
+
+        for(int i=n; i>1;i--){
+            i*=(i-1);
+        }
+
+        return n*Factorial(n-1); 
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(Factorial(n));
+       
+    }
+}
