e09937bb3cc724f6892a49e1c373f81249ce69f0
yde222 <150855134+yde222@users.noreply.github.com>
2025-05-05 15:41:57 +0900

    Title: Top K 숫자 구하기, Time: 167ms, Memory: 12MB, Status: Passed - Codetree

--- 250505/Top K 숫자 구하기/kth-number.java
+++ 250505/Top K 숫자 구하기/kth-number.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+public class Main {    
+    public static final int MAX_N = 1000;
+    
+    // 변수 선언
+    public static int n, k;
+    
+    public static int[] nums = new int[MAX_N];
 
-public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
+        // 입력:
+        n = sc.nextInt();
+        k = sc.nextInt();
+        
+        for(int i = 0; i < n; i++){
             nums[i] = sc.nextInt();
         }
-        Arrays.sort(nums);
-        System.out.print(nums[k-1]);
-        
-    }
 
+        // nums[0]부터 nums[n - 1]까지를 정렬함
+        Arrays.sort(nums, 0, n);
+
+        // k번째 원소를 출력 (원소 k-1)
+        System.out.print(nums[k - 1]);
+
+    }
 }
