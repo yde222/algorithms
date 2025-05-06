55c20d8b69a460a597205542b649b3dbd278ecd7
yde222 <150855134+yde222@users.noreply.github.com>
2025-05-06 22:24:20 +0900

    Title: 진수 to 진수, Time: 113ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250506/진수 to 진수/transformation-of-number-system.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+
+    // public static final MAX_DIGIT=200;
+    // public static int[] digits = new int[MAX_DIGIT];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        String N = sc.next();
+
+        int num = 0;
+        for(int i=0; i< N.length(); i++){
+            num = num*A +(N.charAt(i)-'0');
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(num>0){
+            sb.append(num%B);
+            num /= B;
+        }
+
+        System.out.println(sb.reverse().toString());
+    }
+}
